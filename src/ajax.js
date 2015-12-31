4e114dacf88043f01d91652c98f48fa41f709aae
Jonah Joselow <jonahjoselow@gmail.com>
2015-12-30 20:40:38 -0500

    init

---
+++ src/ajax.js
@@ -0,0 +1,19 @@
+/**
+ * [get description]
+ * @param  {[type]} url [description]
+ * @return {[type]}     [description]
+ */
+export default function get(url){
+  return new Promise((resolve,reject)=>{
+    var ajax=new XMLHttpRequest()
+    ajax.open('GET',url)
+    ajax.onload=(event)=>{
+      if(ajax.status==200){
+        resolve(ajax.response)
+      }else{
+        reject(ajax)
+      }
+    }
+    ajax.send()
+  })
+}
